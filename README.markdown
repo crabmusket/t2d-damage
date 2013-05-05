549da5b0e4c1503715c61479acaa720363514001
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-05-05 15:31:57 +1000

    Initial commit.

---
+++ README.markdown
@@ -0,0 +1,103 @@
+# t2d-damage
+
+Damage behaviors for the [Torque 2D][] game engine.
+
+  [Torque 2D]: https://github.com/GarageGames/Torque2D
+
+# Installation
+
+I recommend cloning this repository right into your T2D folder:
+
+```
+cd modules/
+git clone git@github.com:eightyeight/t2d-damage Damage
+```
+
+Or you can achieve the same effect by [downloading a ZIP file][Download].
+Note that this will download the latest version of the `master` branch, which will be stable
+but may be out of date.
+
+  [Download]: https://github.com/eightyeight/t2d-damage/archive/master.zip
+
+# Use
+
+This module defines a number of behaviors that allow you to create damaging interactions.
+These interactions are modeled as one-way scenarios with an 'attacker' and a 'defender',
+but may have two-way side-effects like damage and physical movement.
+
+## Damageable objects
+
+To make an object damageable, call the `able` function on it:
+
+    Damage.able(%obj, true);
+
+The second argument is a Boolean that defaults to `true` and determines whether or not the object is damageable.
+This gives the object a `damage` property that increases when it takes damage.
+
+## Basic damage
+
+Raw damage can be applied using several functions:
+
+    Damage.just(%obj, %damage);
+    Damage.everythingNear(%obj, %radius, %damage);
+    Damage.everythingLeftOf(%obj, %distance, %damage);
+    Damage.everythingRightOf(%obj, %distance, %damage);
+    Damage.everythingAbove(%obj, %distance, %damage);
+    Damage.everythingBelow(%obj, %distance, %damage);
+
+Except the first method, no damage is applied to `%obj`.
+In the very simplest case, `%damage` can be a number, the amount of damage to add to everything.
+
+However, damage can also be in the form of a string where each word is a damage option.
+For example,
+
+    Damage.just(%obj, "amount:10 type:fire impulseX:50");
+
+specifies the `fire` damage type and a horizontal impulse.
+Note that this module does not define any damage types;
+these are just strings that you can make use of in your own scripts as you deem appropriate.
+
+## Destruction
+
+
+
+## Attackers and defenders
+
+However, sometimes the amount of damage is a complex function of equipment, levels, abilities and the damage scenario.
+This module lets you add all sorts of optional effects to your objects to give attacks (and defence) different properties.
+These effects all model attacks as originating from an _attacker_ and being applied to a _defender_.
+In some cases this is literally true, such as two characters fighting.
+In other cases, you may have a more abstract attacker, such as a spike trap, or a screen region effect.
+
+You can initiate an _attack_ using the appropriate method:
+
+    Damage.attack(%attacker, %defender, %damage);
+
+Again, `%damage` can be a number or an option string.
+There are also some convenience methods that allow you deal damage in common patterns to one or multiple defenders.
+All the `everything` methods can be used with an `attack` prefix, like:
+
+    Damage.attackEverythingNear(%attacker, %range, %damage);
+
+Some common patterns with direction:
+
+    Damage.attackFirstOnLine(%attacker, %direction, %length, %damage);
+    Damage.attackAllOnLine(%attacker, %direction, %length, %damage);
+    Damage.attackAllInArc(%attacker, %direction, %angle, %range, %damage);
+
+In all these cases, `%direction` can be an angle in degrees from the positive _x_ axis, or a vector.
+
+## Effects
+
+Now, the exciting part of this attacker and defender stuff is that either party may have _effects_ applied to them.
+For example, you might pick up a golden lollipop that makes all your attacks do twice the damage.
+This is implemented as an attack multiplier effect.
+
+To add attack and defence effects, use the `effect` method:
+
+    %e = Damage.createAttackEffect();
+    %e.multiplier = 2;
+    %e.duration = 10;
+    Damage.effect(%obj, %e);
+
+For the next 10 seconds, this object's attacks will deal twice the damage.
---
+++ main.cs
@@ -0,0 +1,149 @@
+function Damage::create(%this) {
+   if(!isObject(Damage.Effects)) {
+      Damage.Effects = new SimSet();
+   }
+}
+
+function Damage::destroy(%this) {
+   if(isObject(Damage.Effects)) {
+      Damage.Effects.deleteObjects();
+      Damage.Effects.delete();
+      Damage.Effects = "";
+   }
+}
+
+function Damage::able(%this, %obj, %enable) {
+   if(%enable $= "") {
+      %enable = true;
+   }
+
+   if(%enable && !%obj.isDamageEnabled) {
+      %obj.damage = 0;
+      %obj.isDamageEnabled = true;
+   }
+
+   if(!%enable) {
+      %obj.isDamageEnabled = false;
+   }
+}
+
+function Damage::createAttackEffect(%this, %name) {
+   %bt = new BehaviorTemplate(%name);
+   %bt.damageEffectClass = "AttackEffectBehavior";
+   Damage.Effects.add(%bt);
+}
+
+function Damage::createDefendEffect(%this, %name) {
+   %bt = new BehaviorTemplate(%name);
+   %bt.damageEffectClass = "DefendEffectBehavior";
+   Damage.Effects.add(%bt);
+}
+
+function Damage::effect(%this, %obj, %effect) {
+   %bi = %effect.createInstance();
+   %bi.superClass = %effect.damageEffectClass;
+   %obj.addBehavior(%bi);
+}
+
+function Damage::damage(%this, %attacker, %defender, %dam) {
+   // Parameters that will be filled in later.
+   %types = "";
+   %amount = 0;
+   %impulse = "0 0";
+   %mul = 1;
+   %extra = 0;
+
+   if(getWordCount(%dam) == 1 && strchr(%dam, ":") == "") {
+      %amount = %dam;
+   } else {
+      // Parse options.
+   }
+
+   // Get attack and defence behaviors.
+   %attackB = "";
+   for(%i = 0; %i < %attacker.getBehaviorCount(); %i++) {
+      if(%attacker.getBehaviorByIndex(%i).superClass $= "AttackBehaviorType") {
+         %attackB = %attackB SPC %attacker.getBehaviorByIndex(%i);
+      }
+   }
+   %defendB = "";
+   for(%i = 0; %i < %defender.getBehaviorCount(); %i++) {
+      if(%defender.getBehaviorByIndex(%i).superClass $= "DefendBehaviorType") {
+         %defendB = %defendB SPC %defender.getBehaviorByIndex(%i);
+      }
+   }
+   %bs = %attackB SPC %defendB;
+
+   // Get damage types from the behaviors.
+   foreach$(%b in %bs) { %types = %types SPC %b.getDamageType(%attacker, %defender); }
+   // Get damage modifiers and extra given the type.
+   foreach$(%b in %bs) { %mul *= %b.getDamageMod(%attacker, %defender, %types); %extra += %b.getExtraDamage(%attacker, %defender, %types); }
+
+   // Apply damage multiplier and extra damage.
+   %amount = %amount * %mul + %extra;
+
+   // Get impulse.
+   foreach$(%b in %bs) { %impulse = vectorAdd(%impulse, %b.getImpulse(%attacker, %defender, %types, %amount)); }
+
+   // Apply the damage.
+   %defender.damage += %amount;
+   // Apply the impulse.
+   %defender.applyImpulse(%impulse, %defender.getPosition());
+
+   // Let everyone know about the results.
+   foreach$(%b in %bs) { %b.onDamage(%attacker, %defender, %types, %amount, %impulse); }
+   // Let the object do custom stuff.
+   %defender.onDamage(%attacker, %types, %amount, %impulse);
+}
+
+function DamageEffectBehavior::onBehaviorAdd(%this) {
+   %this.startTime = getSimTime();
+}
+
+function DamageEffectBehavior::onUpdate(%this) {
+   if(%this.duration $= "") {
+      return;
+   }
+
+   // Check duration and end behavior if appropriate.
+   if(getSimTime() - %this.startTime > (%this.duration * 1000)) {
+      %this.owner.schedule(0, removeBehavior, %this);
+      %this.schedule(1000, delete);
+   }
+}
+
+function DamageEffectBehavior::getDamageType(%this, %attacker, %defender) {
+   return "";
+}
+
+function DamageEffectBehavior::getDamageMod(%this, %attacker, %defender, %types) {
+   return 1;
+}
+
+function DamageEffectBehavior::getExtraDamage(%this, %attacker, %defender, %types) {
+   return 0;
+}
+
+function DamageEffectBehavior::getImpulse(%this, %attacker, %defender, %types, %amount) {
+   return "0 0";
+}
+
+function DamageEffectBehavior::onDamage(%this, %attacker, %defender, %types, %amount, %impulse) {
+}
+
+// Ohmygosh.
+function AttackEffectBehavior::onBehaviorAdd(%this) { DamageEffectBehavior::onBehaviorAdd(%this); }
+function AttackEffectBehavior::onUpdate(%this) { DamageEffectBehavior::onUpdate(%this); }
+function AttackEffectBehavior::getDamageType(%this, %a, %d) { return DamageEffectBehavior::getDamageType(%this, %a, %d); }
+function AttackEffectBehavior::getDamageMod(%this, %a, %d, %t) { return DamageEffectBehavior::getDamageMod(%this, %a, %d, %t); }
+function AttackEffectBehavior::getExtraDamage(%this, %a, %d, %t) { return DamageEffectBehavior::getExtraDamage(%this, %a, %d, %t); }
+function AttackEffectBehavior::getImpulse(%this, %a, %d, %t, %amnt) { return DamageEffectBehavior::getImpulse(%this, %a, %d, %t, %amnt); }
+function AttackEffectBehavior::onDamage(%this, %a, %d, %t, %amnt, %imp) { return DamageEffectBehavior::onDamage(%this, %a, %d, %t, %amnt, %imp); }
+function DefendEffectBehavior::onBehaviorAdd(%this) { DamageEffectBehavior::onBehaviorAdd(%this); }
+function DefendEffectBehavior::onUpdate(%this) { DamageEffectBehavior::onUpdate(%this); }
+function DefendEffectBehavior::getDamageType(%this, %a, %d) { return DamageEffectBehavior::getDamageType(%this, %a, %d); }
+function DefendEffectBehavior::getDamageMod(%this, %a, %d, %t) { return DamageEffectBehavior::getDamageMod(%this, %a, %d, %t); }
+function DefendEffectBehavior::getExtraDamage(%this, %a, %d, %t) { return DamageEffectBehavior::getExtraDamage(%this, %a, %d, %t); }
+function DefendEffectBehavior::getImpulse(%this, %a, %d, %t, %amnt) { return DamageEffectBehavior::getImpulse(%this, %a, %d, %t, %amnt); }
+function DefendEffectBehavior::onDamage(%this, %a, %d, %t, %amnt, %imp) { return DamageEffectBehavior::onDamage(%this, %a, %d, %t, %amnt, %imp); }
+
